53b52a018d17301a8c4449aa82f4fc7669d41c2e
gilzoide <gilzoide@gmail.com>
2020-04-27 12:04:48 -0300

    Add movimento do Piao com input

--- piao/Piao.gd
+++ piao/Piao.gd
@@ -1,10 +1,29 @@
-extends RigidBody
+extends KinematicBody
 
-export(float) var speed = 30
+export(float) var move_speed = 10
+export(float) var rotation_speed = 500
 
+var move_to: Vector3
 
 func _ready():
-	apply_torque_impulse(Vector3.DOWN * 100)
+	set_process(true)
 
-#func _physics_process(delta):
-#	rotate_object_local(Vector3.DOWN, delta * speed)
+func _process(delta):
+	var moving_to = Vector3.ZERO
+	if Input.is_action_pressed("ui_up"):
+		moving_to.z = -1
+	elif Input.is_action_pressed("ui_down"):
+		moving_to.z = 1
+	if Input.is_action_pressed("ui_left"):
+		moving_to.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		moving_to.x = 1
+	animate(moving_to, delta)
+	apply_move(moving_to, delta)
+
+func animate(moving_to: Vector3, delta: float):
+	$Pivot/Mesh.rotate_object_local(Vector3.DOWN, rotation_speed * delta)
+	$Pivot.rotation = Vector3(deg2rad(moving_to.z * 30), 0, deg2rad(-moving_to.x * 30))
+
+func apply_move(moving_to: Vector3, delta: float):
+	var _x = move_and_collide(moving_to * move_speed * delta)
